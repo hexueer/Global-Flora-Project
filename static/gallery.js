68d1cfca394765709e2f71a33635d50f65fe4a51
team Ivy Ho <iho@wellesley.edu>
2022-03-30 01:22:41 -0400

    map filter added

--- static/gallery.js
+++ static/gallery.js
@@ -1,10 +1,15 @@
 var display = photos; // photos dictionary loaded as var in gallery.html
+var firstLoadPhotoNum = 16;
+var photoCounter = 0;
+var start;
 var photo;
-
-// populate the gallery with max 12 images depending on the display of photos provided
-function populateGallery(display) {
-  for (let i = 0; i < Math.min(display.length, 12); i++) {
+// Math.min(display.length - start, max)
+// populate the gallery with a max number of images depending on the display of photos provided
+function populateGallery(display, max) {
+  start = photoCounter;
+  for (let i = start; i < max; i++) {
     photo = display[i];
+    console.log(i);
     $('.img-gallery-container').append('<div class="gallery-container"><div class="gallery-item"><div class="image"><img id="' + i + '" src="" alt="" data-season="" data-month="" data-name="" data-original=""></div></div></div>');
     $("#"+i).attr('src', photo.ImageFile);
     $("#"+i).attr('alt', photo.AltText);
@@ -12,13 +17,15 @@ function populateGallery(display) {
     $("#"+i).attr('data-month', photo.Month);
     $("#"+i).attr('data-name', photo.Name);
     $("#"+i).attr('data-original', photo.ImageFile);
+    photoCounter = i + 1;
   };
+
+  // preview feature
+  addModal();
 };
 
-
-
+//image gallery modals (when image is clicked, the full sized image will appear)
 function addModal() {
-  //image gallery modals (when image is clicked, the full sized image will appear)
   const modal = document.querySelector(".modal");
   const previews = document.querySelectorAll(".gallery-item .image img");
   const original = document.querySelector(".modal-img");
@@ -44,8 +51,8 @@ function addModal() {
 };
 
 // automatically display 12 most recently uploaded photos
-populateGallery(photos);
-addModal();
+populateGallery(display, firstLoadPhotoNum);
+populateGallery(display, 2);
 
 // functions to display filtered selection of photos according to tag category
 function filterBySeason(season) {
@@ -56,8 +63,7 @@ function filterBySeason(season) {
     display = $.map(photos, function(photo) { if (photo.Season == season) {return photo;} });
     
     // repopulate gallery with preview feature
-    populateGallery(display);
-    addModal(); 
+    populateGallery(display, firstLoadPhotoNum);
 };
 
 function filterByMonth(month) {
@@ -68,14 +74,29 @@ function filterByMonth(month) {
   display = $.map(photos, function(photo) { if (photo.Month == month) {return photo;} });
   
   // repopulate gallery with preview feature
-  populateGallery(display);
-  addModal();
+  populateGallery(display, firstLoadPhotoNum);
 };
 
-// load photo dictionary into this file as js object
-// limit number of images initial
-// complete list, scroll event fill in photo data(url), dynamically create image objects
-// onscroll event
-// use js and jQuery to set links/other details for each img tag
-// use click events to trigger filter functions
-// event handler for form
+function filterByArea(area) {
+  $('.img-gallery-container').empty(); // empty out gallery
+  display = []; // empty out display data
+
+  // create new dictionary of photos for display according to chosen season
+  display = $.map(photos, function(photo) { if (photo.Area == area) {return photo;} });
+  
+  // repopulate gallery with preview feature
+  populateGallery(display, firstLoadPhotoNum);
+};
+
+// $(".blah").on("click", function (e) {
+//   e.preventDefault();
+//   filterBySeason('blah');
+// });
+
+// dynamically generate more img elements on scroll
+// $(".gallery-container-color").scroll(function () {
+//   if($(this).scrollTop() + 1 >= $(".img-gallery-container").height() - $(".gallery-container-color").height()) {
+//     // $( ".img-gallery-container" ).append( "<p>Hello</p>" );
+//     populateGallery(display, 4);
+//   }
+// });
--- static/map.js
+++ static/map.js
@@ -3,6 +3,7 @@ $(".map-CP").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-CP.png";
+    filterByArea('Camellia Pavilion');
 });
 
 //change color of Mezzanine
@@ -10,20 +11,15 @@ $(".map-M").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-M.png";
+    filterByArea('Mezzanine');
 });
 
-//change color of West North
+//change color of Wet North
 $(".map-WN").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-WN.png";
-});
-
-//change color of Dry North
-$(".map-DN").on("click", function (e) {
-    e.preventDefault();
-    console.log("clicked!");
-    document.getElementById('map').src = "../static/img/map-images/Map-Clicked-DN.png";
+    filterByArea('Wet North');
 });
 
 //change color of Wet West
@@ -31,6 +27,7 @@ $(".map-WW").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-WW.png";
+    filterByArea('Wet West');
 });
 
 //change color of Wet East
@@ -38,6 +35,7 @@ $(".map-WE").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-WE.png";
+    filterByArea('Wet East');
 });
 
 //change color of Wet South
@@ -45,6 +43,15 @@ $(".map-WS").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-WS.png";
+    filterByArea('Wet South');
+});
+
+//change color of Dry North
+$(".map-DN").on("click", function (e) {
+    e.preventDefault();
+    console.log("clicked!");
+    document.getElementById('map').src = "../static/img/map-images/Map-Clicked-DN.png";
+    filterByArea('Dry North');
 });
 
 //change color of Dry West
@@ -52,6 +59,7 @@ $(".map-DW").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-DW.png";
+    filterByArea('Dry West');
 });
 
 //change color of Dry East
@@ -59,6 +67,7 @@ $(".map-DE").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-DE.png";
+    filterByArea('Dry East');
 });
 
 //change color of Dry South
@@ -66,4 +75,5 @@ $(".map-DS").on("click", function (e) {
     e.preventDefault();
     console.log("clicked!");
     document.getElementById('map').src = "../static/img/map-images/Map-Clicked-DS.png";
+    filterByArea('Dry South');
 });
